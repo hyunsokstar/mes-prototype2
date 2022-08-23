2530975ac3d87a86b251a12a79a7953d222f1302
ohyunsok <hyunsokstar@gmail.com>
2022-08-23 18:09:19 +0900

    update for add search bar

--- front/components/SearchBox/index.tsx
+++ front/components/SearchBox/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { AudioOutlined } from '@ant-design/icons';
-import { Input, Space } from 'antd';
 
 
 type IProps = {}
 
-function SearchBox({}: IProps) {
+function SearchBox({ }: IProps) {
   return (
-    <div>
-      <select name="" id="">
-
+    <div style={{float:"right"}}>
+      <select name="cars" id="cars">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="opel">Opel</option>
+        <option value="audi">Audi</option>
       </select>
-      <input type="text" placeholder='검색어 입력' width={80}/>
+      <input type="text" placeholder='검색어 입력' width={80} />
     </div>
   )
 }
